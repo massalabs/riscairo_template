18777dfa7fd37350b05f8ba408b09ef5ddbe852b
Damir Vodenicarevic <damipator@gmail.com>
2024-08-06 13:38:20 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+echo "Building rust guest..."
+cd guest_rs
+cargo clean
+cargo build --release
+cd ..
+echo "Converting elf files..."
+python3 convert.py
+echo "Compiling cairo host..."
+cd host_cairo
+scarb clean
+RUST_MIN_STACK=5000000 scarb --release build
+cd ..
+echo "Build successful."
+
